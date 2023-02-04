fa6acc26b570b46be6421b36fa1df63f75a09f8b
Tom Price <tomgprice90@gmail.com>
2023-02-04 08:53:01 +0000

    import packages, data and creat graph variables

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+
+
+
+#import the dataset
+df = pd.read_csv("Data.csv")
+
+
+#######################
+### Graph Variables ###
+#######################
+
+MARGIN = {"t": 50, "r": 200, "b": 40, "l": 40}
+LINE_COLOUR = "#A6ACAF"
+PRIMARY_COLOUR = "#1F618D"
+FONT_COLOR = "#6d7578"
+TITLE_COLOUR = "#303436"
+
+
+
+
+
